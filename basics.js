00970e5ee233407a18ac10e8683764a535561a6f
rudissh <rudisshkanna.1199@gmail.com>
2022-09-10 14:21:06 +0530

    Javascript Concepts

---
+++ basics.js
@@ -0,0 +1,66 @@
+// we can use console and its methods for debugging
+/**
+ * Variable declaration:
+ * var,let and const
+ * var - old way of declaration and it is global scoped
+ * let and const - they are added with ES6 and have block-level scope, const variables cannot be chaged
+ */
+
+/**
+ * DataTypes:
+ * Primitive:
+ * String,Number,Boolean,null,undefined,Symbol
+ * null in typeof() is bogus value
+ * String can be concatenated with "+" variables to form a string or variables can be injected into a template string with `${}` from ES6
+ */
+
+/**
+ * Arrays:
+ * Variables that hold mutliple values
+ * const numbers = new Array(1,2,3);
+ * const fruits = ['apples, 'oranges', 'pears', 10, true]
+ * Arrays are dynamic and can hold different daatype values
+ * Arrays are always zero based indexing
+ */
+
+/**
+ * Object literals: 
+ * objects with key value pairs
+ * we can destructure objets to get values
+ * JSON is very similar to 
+ */
+
+/**
+ * Prototype OOPS
+ * We have proto and constructor
+ */
+
+// function Person(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.dob = new Date(dob);
+// }
+
+// Person.prototype.getBirthYear = () => {
+//     return this.dob.getFullYear();
+// }
+
+// const person1 = new Person('John', 'Doe', '4-3-2020');
+
+// console.log
+
+/**
+ * Classes 
+ * They are a syntactical sugar to the prototypal OOPS 
+ */
+
+/**
+ * DOM: 
+ * Selection:
+ * document.querySelector(), querySelectorAll()
+ *  Changing Content:
+ * textContent, innerText, innerHTML
+ * 
+ * EVents:
+ * addEventListener('event', callback )
+ */
